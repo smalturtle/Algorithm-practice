ac21035a5f8bf0f28830085510f88541065bfbd0
smalturtle <3372017639@qq.com>
2026-09-05 10:21:05 +0800

    likou-131

---
+++ tm/2609051019.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ * 力扣131，分割回文串
+ * 
+ */
+
+
+class Solution {
+    List<List<String>> ret = new ArrayList<>();
+
+    List<String> path = new ArrayList<>();
+
+    public List<List<String>> partition(String s) {
+        work(s);
+        return ret;
+    }
+    public void work(String s){
+
+        //切割完毕（字符串被切空了），那就可以加入结果里了
+        if(s.equals("")){
+            ret.add(new ArrayList<>(path));
+            return ;
+        }
+        //枚举切割的位置，注意必须是 <= s.length()
+        for(int i =1;i<=s.length();i++){
+            String left = s.substring(0,i);
+            String right = s.substring(i,s.length());
+            //前半段是回文，才继续切后半段
+            if(isPail(left)){
+                path.add(left.toString());
+                work(right);
+                path.remove(path.size() -1);
+            }
+        }
+    }
+    public boolean isPail(String s){
+        int left = 0,right = s.length()-1;
+        while(left <right){
+            if(s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
