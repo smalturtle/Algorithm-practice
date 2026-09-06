4b5e582c52868dcfe97bcf18cf03cfb03dc5650c
smalturtle <3372017639@qq.com>
2026-09-06 15:09:06 +0800

    likou-35

---
+++ tm/month9/2609061508.java
@@ -0,0 +1,19 @@
+/**
+ * 力扣35，搜索插入位置
+ * 
+ */
+
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0,right = nums.length;
+
+        while(left < right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] < target){
+                left = mid+1;
+            }else right = mid;
+        }
+        return left;
+    }
+}
